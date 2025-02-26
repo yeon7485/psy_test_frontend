47f58661fa40c97e51de1d3f359253aa23a4913d
seiyeon <wsx4379@naver.com>
2025-02-26 14:42:42 +0900

    [Feat] 댓글 기능 구현
    댓글 기능 구현

--- src/components/CommentCard.vue
+++ src/components/CommentCard.vue
@@ -10,10 +10,32 @@ const props = defineProps({
 </script>
 
 <template>
-  <div>
-    <p>{{ comment.content }}</p>
-    <p>{{ comment.writer }}</p>
+  <div class="comment-card">
+    <p class="comment-writer">작성자 : {{ comment.writer }}</p>
+    <p class="comment-content">{{ comment.content }}</p>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.comment-card {
+  background-color: #f9f9f9; /* 배경색 */
+  border-radius: 8px; /* 둥근 테두리 */
+  padding: 1.2rem 1.5rem;
+  margin-bottom: 10px;
+  box-shadow: 0 1px 4px rgba(0, 0, 0, 0.1); /* 가벼운 그림자 */
+}
+
+.comment-writer {
+  font-weight: bold;
+  color: #333;
+  margin-bottom: 3px;
+  font-size: 0.8rem;
+  margin: 0 0 0.5rem 0;
+}
+
+.comment-content {
+  color: #555;
+  font-size: 14px;
+  margin: 0;
+}
+</style>
--- src/stores/useBoardStore.js
+++ src/stores/useBoardStore.js
@@ -5,20 +5,46 @@ const backend = "/api";
 
 export const useBoardStore = defineStore("board", {
   actions: {
+    // 게시글 전체 조회
     async getBoardList(page) {
       const response = await axios.get(
         backend + "/board/list?page=" + page + "&size=10"
       );
 
-      return response.data.data;
+      if (response.status === 200) {
+        return response.data.data;
+      } else {
+        return false;
+      }
     },
+
+    // 게시글 상세 조회
     async getBoardDetail(idx) {
       const response = await axios.get(backend + "/board/read/" + idx);
 
-      return response.data.data;
+      if (response.status === 200) {
+        return response.data;
+      } else {
+        return false;
+      }
     },
+
+    // 게시글 작성
     async createBoard(boardData) {
       const response = await axios.post(backend + "/board/create", boardData);
+      if (response.status === 200) {
+        return response.data;
+      } else {
+        return false;
+      }
+    },
+
+    // 댓글 작성
+    async addComment(boardIdx, commentData) {
+      const response = await axios.post(
+        backend + "/board/comment/" + boardIdx,
+        commentData
+      );
       if (response.status === 200) {
         return response.data;
       } else {
