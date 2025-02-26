475c61bef6dcaaaa0c8cb6511c8d5cf62e8db1de
seiyeon <wsx4379@naver.com>
2025-02-26 14:53:13 +0900

    [Fix] router 경로 수정
    router 경로 수정

---
+++ src/pages/MainPage.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const goToBoard = () => {
+  router.push("/board");
+};
+</script>
+
+<template>
+  <div class="container">
+    <button @click="goToBoard" class="btn">게시판 바로가기</button>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  width: 100%;
+  height: 80vh;
+  justify-content: center;
+  align-items: center;
+}
+
+.btn {
+  padding: 1rem 4rem;
+  background-color: slateblue;
+  color: white;
+  border: none;
+  font-size: 1.2rem;
+  border-radius: 0.8rem;
+  transition: background-color 0.3s;
+}
+
+.btn:hover {
+  background-color: darkslateblue;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,11 @@ import { createRouter, createWebHistory } from "vue-router";
 import BoardPage from "../pages/BoardPage.vue";
 import BoardDetail from "../pages/BoardDetail.vue";
 import CreateBoard from "../pages/CreateBoard.vue";
+import MainPage from "../pages/MainPage.vue";
 
 const routes = [
-  { path: "/", component: BoardPage },
+  { path: "/", component: MainPage },
+  { path: "/board", component: BoardPage },
   { path: "/board/:idx", component: BoardDetail },
   { path: "/board/create", component: CreateBoard },
 ];
